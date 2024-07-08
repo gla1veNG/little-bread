e376553f9117dbb31a535efa924f124ee7a56cb5
Ji7mmy <2993078435@qq.com>
2024-07-08 16:29:25 +0800

    添加 toggleAside，让 topnav 是响应式

--- Bread-ui/src/components/Topnav.vue
+++ Bread-ui/src/components/Topnav.vue
@@ -5,6 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 <script lang="ts">
@@ -12,7 +13,7 @@ import {inject, Ref} from 'vue';
 
 export default {
   setup() {
-    const asideVisible = inject<Ref<boolean>>('xxx');
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
     const toggleMenu = () => {
       asideVisible.value = !asideVisible.value;
     };
@@ -27,6 +28,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-content: center;
 
   > .logo {
     max-width: 6em;
@@ -40,6 +43,27 @@ export default {
 
     > li {
       margin: 0 1em;
+    }
+  }
+
+  > .toggleAside {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+
+  @media (max-width: 500px) {
+    > .menu {
+      display: none
+    }
+    > .logo {
+      margin: 0 auto
     }
   }
 }
