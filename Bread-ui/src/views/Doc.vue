bdca4d3bdfbcb31fc2a68fe52f9236c555566ce9
Ji7mmy <2993078435@qq.com>
2024-07-08 15:53:25 +0800

    添加了 aside 的样式，调节 Topnav 和 aside 的位置

--- Bread-ui/src/views/Doc.vue
+++ Bread-ui/src/views/Doc.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-   <Topnav/>
+    <Topnav/>
     <div class="containe">
       <aside>
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/swich" >Swich组件</router-link>
+            <router-link to="/doc/swich">Swich组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/button" >Buttom组件</router-link>
+            <router-link to="/doc/button">Buttom组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/dialog" >Dialog组件</router-link>
+            <router-link to="/doc/dialog">Dialog组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/tabs" >Tabs组件</router-link>
+            <router-link to="/doc/tabs">Tabs组件</router-link>
           </li>
         </ol>
       </aside>
@@ -26,11 +26,30 @@
 
 <script lang="ts">
 import Topnav from '../components/Topnav.vue';
+
 export default {
   components: {Topnav}
-}
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
 
+  > h2 {
+    margin-bottom: 4px;
+  }
+
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
