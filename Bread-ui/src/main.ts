ae5a0dcfd06fd34a0216fd034dea5d7c3f901f2f
Ji7mmy <2993078435@qq.com>
2024-07-08 15:15:29 +0800

    添加了 Home 的样式

--- Bread-ui/src/main.ts
+++ Bread-ui/src/main.ts
@@ -1,20 +1,20 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import './index.css'
-import {createWebHashHistory, createRouter} from "vue-router";
-import Frank from "./components/Frank.vue"
-import Frank2 from "./components/Frank2.vue"
+import {createApp} from 'vue';
+import App from './App.vue';
+import './index.scss';
+import {createWebHashHistory, createRouter} from 'vue-router';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
 
 const history = createWebHashHistory();
 const router = createRouter({
         history: history,
         routes: [
-            {path: '/', component:Frank},
-            {path: '/xxx', component:Frank2},
+            {path: '/', component: Home},
+            {path: '/doc', component: Doc},
 
         ]
     }
 );
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
