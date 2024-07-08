f2627823b4ac610dd8fb75e7a68a4c8606e73cdf
Ji7mmy <2993078435@qq.com>
2024-07-08 16:15:19 +0800

    用 provide 和 inject 实现切换功能

--- Bread-ui/src/components/Topnav.vue
+++ Bread-ui/src/components/Topnav.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo" @click="toggleMenu">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
   </div>
 </template>
+<script lang="ts">
+import {inject, Ref} from 'vue';
+
+export default {
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('xxx');
+    const toggleMenu = () => {
+      asideVisible.value = !asideVisible.value;
+    };
+    return {toggleMenu};
+  },
+};
+</script>
 <style lang="scss" scoped>
 .topnav {
   background: pink;
@@ -14,6 +27,7 @@
   padding: 16px;
   position: relative;
   z-index: 10;
+
   > .logo {
     max-width: 6em;
     margin-right: auto;
