c47a812ca9aaeeff0d9675f1c3e73e93945dedb0
Ji7mmy <2993078435@qq.com>
2024-07-09 14:59:37 +0800

    完善 switch 的样式

--- Bread-ui/src/lib/Switch.vue
+++ Bread-ui/src/lib/Switch.vue
@@ -26,7 +26,7 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: #a6a6a6;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
 }
@@ -39,12 +39,18 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2/2;
-  transition: left 0.5s;
+  transition: all 0.5s;
 }
 button.checked{
   background: #42d342;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:active{
+  >span{width:$h2+4px;}
+}
+button.checked:active{
+  >span{width:$h2+4px;margin-left:-4px;}
 }
 </style>
