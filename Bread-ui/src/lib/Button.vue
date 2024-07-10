c321e8ff37a7d1aad693738ca6f5dc443901349e
Ji7mmy <2993078435@qq.com>
2024-07-10 14:15:30 +0800

    支持 level 属性

--- Bread-ui/src/lib/Button.vue
+++ Bread-ui/src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
     size:{
       type:String,
       default:'normal',
+    },
+    level:{
+      type:String,
+      default:'normal',
     }
   },
   setup(props){
-    const {theme,size} = props;
+    const {theme,size,level} = props;
       const classes = computed(()=>{
       return {
         [`bread-theme-${theme}`]:theme,
         [`bread-size-${size}`]:size,
+        [`bread-level-${level}`]:level,
       }
     })
     return {classes}
@@ -37,6 +42,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red:red;
 .bread-button {
   box-sizing: border-box;
   height: $h;
@@ -51,6 +57,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -82,17 +89,63 @@ $radius: 4px;
       background: darken(white, 5%);
     }
   }
-  &.bread-theme-button{
-    &.bread-size-big{
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px
+  &.bread-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.bread-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.bread-theme-button {
+    &.bread-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.bread-size-small{
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+    &.bread-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.bread-theme-link {
+    &.bread-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.bread-theme-text {
+    &.bread-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.bread-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
 }
+
 </style>
