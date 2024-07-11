b423378f61b344586c94bd5b948486b7a775ba07
Ji7mmy <2993078435@qq.com>
2024-07-11 14:24:31 +0800

    Dialog 支持点击关闭

--- Bread-ui/src/components/DialogDemo.vue
+++ Bread-ui/src/components/DialogDemo.vue
@@ -4,10 +4,10 @@
   </div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog :visible="x"></Dialog>
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
-<script  lang="ts">
+<script lang="ts">
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
 import {ref} from 'vue';
@@ -15,11 +15,16 @@ import {ref} from 'vue';
 export default {
   components: {Button, Dialog},
   setup() {
-      const x = ref(false);
-      const toggle = ()=>{
-        x.value = !x.value;
-      }
-      return {x,toggle}
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false
+    };
+    const f2 = () => {
+    };
+    return {x, toggle, f1, f2};
   },
 };
 </script>
