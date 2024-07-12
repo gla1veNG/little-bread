cd7fe1808a718d646c69658ea27b80236e104050
Ji7mmy <2993078435@qq.com>
2024-07-12 14:32:07 +0800

    Tabs 代码优化1

--- Bread-ui/src/lib/Tabs.vue
+++ Bread-ui/src/lib/Tabs.vue
@@ -3,7 +3,7 @@
     <div class="bread-tabs-nav" ref="container">
       <div class="bread-tabs-nav-item" :class="{selected:t === selected}"
            v-for="(t,index) in titles" :key="index"
-           :ref="el =>{ if(el) navItems[index] = el}"
+           :ref="el =>{ if(t === selected) selectedItem = el}"
            @click="select(t)">{{ t }}</div>
 
       <div class="bread-tabs-nav-indicator" ref="indicator"></div>
@@ -24,16 +24,14 @@ export default {
     selected: {type: String}
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = ()=>{
-      const divs = navItems.value;
-      const result = divs.filter(div=>div.classList.contains('selected'))[0];
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {left:left1} = container.value.getBoundingClientRect();
-      const {left:left2} = result.getBoundingClientRect();
+      const {left:left2} = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + 'px';
     }
@@ -52,7 +50,7 @@ export default {
     const select = (title: String) => {
       context.emit('update:selected', title);
     };
-    return {defaults, titles, select,navItems,indicator,container};
+    return {defaults, titles, select,selectedItem,indicator,container};
   }
 };
 </script>
