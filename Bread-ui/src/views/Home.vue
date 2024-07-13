2176457bee16ccdf12819ca55b92fa1aff72a667
Ji7mmy <2993078435@qq.com>
2024-07-13 12:38:25 +0800

    从 iconfont.cn 引入图标添加了首页的 logo 和 icon

--- Bread-ui/src/views/Home.vue
+++ Bread-ui/src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="topnavAndBanner">
-    <Topnav/>
-    <div class="banner">
-      <h1>面板UI</h1>
-      <h2>一个厉害的UI框架</h2>
-      <p class="actions">
-        <a href="https://github.com/gla1veNG/little-bread">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>面板UI</h1>
+        <h2>一个厉害的UI框架</h2>
+        <p class="actions">
+          <a href="https://github.com/gla1veNG/little-bread">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon">
+        <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-Ts"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-light"></use>
+      </svg>
     </div>
   </div>
 </template>
@@ -20,31 +33,42 @@ export default {
 };
 </script>
 
-<style lang="scss" >
-.topnavAndBanner{
-
+<style lang="scss">
+$green: #02bcb0;
+$border-radius: 5px;
+$color: #007974;
+.topnavAndBanner {
+  background: rgb(227, 255, 253);
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
+.features{
+  >svg{
+    width: 64px;
+    height: 64px;
+  }
 }
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: rgb(227,255,253);
-  background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
 
   > .actions {
     padding: 8px 0;
 
     a {
       margin: 0 8px;
-      background: #fff;
+      color: white;
+      background: $green;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
