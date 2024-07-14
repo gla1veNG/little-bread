4b43f4fdf9bbf0b9282c8313898352117fb4810a
Ji7mmy <2993078435@qq.com>
2024-07-14 18:43:59 +0800

    抽离用户需要拷贝的代码

--- Bread-ui/src/components/SwitchDemo.vue
+++ Bread-ui/src/components/SwitchDemo.vue
@@ -1,22 +1,62 @@
 <template>
   <div>
-    <Switch v-model:value="bool"/>
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <Switch1Demo/>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <Switch2Demo/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue';
-import {ref} from 'vue';
-
+import Switch from '../lib/Switch.vue'
+import Button from '../lib/Button.vue'
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
+import {ref} from 'vue'
 export default {
-  components: {Switch},
-  setup() {
-      const bool = ref(false);
-      return {bool}
+  components: {
+    Switch1Demo,
+    Switch2Demo,
+    Switch,
+    Button
   },
-};
+  setup() {
+    const bool = ref(false)
+    return {bool}
+  }
+}
 </script>
 
-<style scoped lang="scss">
-
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>
