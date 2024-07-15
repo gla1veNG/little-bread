6af533f0861c40fc1f6912525060d2a492f013e3
Ji7mmy <2993078435@qq.com>
2024-07-15 11:27:41 +0800

    改造完成 DialogDemo

--- Bread-ui/src/components/DialogDemo.vue
+++ Bread-ui/src/components/DialogDemo.vue
@@ -1,52 +1,26 @@
 <template>
   <div>
-    Dialog示例
-  </div>
-  <h1>示例1</h1>
-  <div>
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="x" :closeOnClickOverlay="x" :ok="f1" :cancel="f2">
-      <template v-slot:title>
-        <strong>加粗的标题</strong>
-      </template>
-      <template v-slot:content>
-        <strong>hi1</strong>
-        <div>hi2</div>
-      </template>
-    </Dialog>
-    <h2>示例2</h2>
-    <Button @click="showDialog">show</Button>
+    <h1>Dialog 示例</h1>
+    <Demo :component="Dialog1Demo"/>
+    <Demo :component="Dialog2Demo"/>
   </div>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue';
-import Button from '../lib/Button.vue';
-import {openDialog} from '../lib/openDialog';
-import {ref} from 'vue';
+import Demo from './Demo.vue';
+import Dialog1Demo from './Dialog1.demo.vue';
+import Dialog2Demo from './Dialog2.demo.vue';
 
 export default {
-  components: {Button, Dialog},
-  setup() {
-    const x = ref(false);
-    const toggle = () => {
-      x.value = !x.value;
-    };
-    const f1 = () => {
-      return x;
-    };
-    const f2 = () => {
-    };
-    const showDialog = () => {
-      openDialog({
-        title: '标题', content: '你好', ok() {console.log('ok');},cancel() {console.log('cancel');}
-      });
-    };
-    return {x, toggle, f1, f2, showDialog};
+  components: {
+    Demo
   },
+  setup() {
+
+    return {
+      Dialog1Demo,
+      Dialog2Demo
+    };
+  }
 };
 </script>
-
-<style scoped lang="scss">
-
-</style>
